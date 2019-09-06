72cca75a7ac978e57fc381a13c0de9b1b51b823b
Emanuel Miller <me@emanuelmiller.com>
2019-09-06 15:44:11 -0700

    OPS-817

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -42,7 +42,6 @@ log_pooler_errors = ${PGBOUNCER_LOG_POOLER_ERRORS:-1}
 stats_period = ${PGBOUNCER_STATS_PERIOD:-60}
 ignore_startup_parameters = ${PGBOUNCER_IGNORE_STARTUP_PARAMETERS}
 query_wait_timeout = ${PGBOUNCER_QUERY_WAIT_TIMEOUT:-120}
-stats_period = ${PGBOUNCER_STATS_PERIOD:-60}
 
 [databases]
 EOFEOF
